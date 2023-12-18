3cb95ca014b21f9b2fd408cba19e71d98a8db2e1
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-12-18 16:27:29 +0300

    Error response class with String message created

---
+++ src/main/java/com/workintech/burger/exceptions/BurgerErrorResponse.java
@@ -0,0 +1,14 @@
+package com.workintech.burger.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class BurgerErrorResponse {
+    private String message;
+}
