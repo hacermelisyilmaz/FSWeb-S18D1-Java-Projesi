a9e354e6109a301d732847ac118582ceb9d4936b
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-12-18 16:26:20 +0300

    Mappings

--- src/main/java/com/workintech/burger/controller/BurgerController.java
+++ src/main/java/com/workintech/burger/controller/BurgerController.java
@@ -2,6 +2,7 @@ package com.workintech.burger.controller;
 
 import com.workintech.burger.dao.BurgerDao;
 import com.workintech.burger.entity.Burger;
+import com.workintech.burger.util.BreadType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,12 +26,37 @@ public class BurgerController {
 
     @GetMapping("/{id}")
     public Burger findById(@PathVariable Long id) {
-        Optional<Burger> burger = burgerDao.findById(id);
+        Optional<Burger> burger = Optional.ofNullable(burgerDao.findById(id));
         return burger.orElse(null);
     }
 
     @PostMapping
-    public Burger save(Burger burger) {
+    public Burger save(@RequestBody Burger burger) {
         return burgerDao.save(burger);
+    }
+
+    @PutMapping("/{id}")
+    public Burger update(@PathVariable Long id, @RequestBody Burger burger) {
+        return burgerDao.update(burger);
+        /*where to use id*/
+    }
+
+    @DeleteMapping("/{id}")
+    public Burger remove(@PathVariable Long id) {
+        return burgerDao.remove(id);
+    }
+
+    @GetMapping("/findByPrice")
+    public List<Burger> findByPrice(Double price) {
+        return burgerDao.findByPrice(price);
+    }
+
+    @GetMapping("/findByBreadType")
+    public List<Burger> findByBreadType(BreadType breadType) {
+        return burgerDao.findByBreadType(breadType);
+    }
+    @GetMapping("/findByContent")
+    public List<Burger> findByContent(String content) {
+        return burgerDao.findByContent(content);
     }
 }
