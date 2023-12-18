e4e7c3992cb97ba9463bb2944b926d1ebb8fcc0c
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-12-18 16:15:33 +0300

    @GetMapping for findAll and findById

--- src/main/java/com/workintech/burger/controller/BurgerController.java
+++ src/main/java/com/workintech/burger/controller/BurgerController.java
@@ -1,4 +1,34 @@
 package com.workintech.burger.controller;
 
+import com.workintech.burger.dao.BurgerDao;
+import com.workintech.burger.entity.Burger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/burgers")
 public class BurgerController {
+    private BurgerDao burgerDao;
+
+    @Autowired
+    public BurgerController(BurgerDao burgerDao) {
+        this.burgerDao = burgerDao;
+    }
+
+    @GetMapping
+    public List<Burger> findAll() {
+        return burgerDao.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Burger findById(@PathVariable Long id) {
+        Optional<Burger> burger = burgerDao.findById(id);
+        return burger.orElse(null);
+    }
 }
